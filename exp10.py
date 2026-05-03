408f8c41fcf45c7fed6d321233907c604bd8008d
Aman Kumar Sharma <143068036+coderXaman@users.noreply.github.com>
2026-05-03 23:55:32 +0530

    Create exp10.py

---
+++ exp10.py
@@ -0,0 +1,11 @@
+import cv2 
+import matplotlib.pyplot as plt 
+ 
+img = cv2.imread('input_image.jpg', 0) 
+edges = cv2.Canny(img, 100, 200) 
+ 
+plt.subplot(1,2,1); plt.imshow(img, cmap='gray'); plt.title('Original'); 
+plt.axis('off') 
+plt.subplot(1,2,2); plt.imshow(edges, cmap='gray'); plt.title('Edges'); 
+plt.axis('off') 
+plt.show()
